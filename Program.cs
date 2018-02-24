9f25aff082e04a87918b38fea74d9aa5997e3cd6
Stepan Bechynsky <stepan@bechynsky.cz>
2018-02-24 16:12:31 +0100

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BC2Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
